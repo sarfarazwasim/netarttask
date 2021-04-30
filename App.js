b3f3459c9636a10ad3d4b6c300a1fd1ddac5d209
sarfarazwasim <72258873+sarfarazwasim@users.noreply.github.com>
2021-04-30 10:39:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,62 @@
+import logo from "./Netart/asserts/logo.png";
+import img1 from "./Netart/asserts/1.png";
+import img2 from "./Netart/asserts/2.png";
+import img3 from "./Netart/asserts/3.png";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+        <div >
+        <img src={logo} className="logo"></img>
+
+        <div className="ab">
+        <div className="abc">
+        
+        <img src={img1} className="img1"></img>
+          <div className="mar">
+          <span className="t1">C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</span>
+         <div className="t2"> <ul >
+              <li>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for various projects across the globe to save energy.</li>
+              <li>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel. </li>
+        </ul></div>
+        <img src={img2} className="img2"></img>
+        <div>
+        <div className="t3">Government of India has awarded the <span className="t4">"National Energy Conservation Award 2018"</span>. Mr. G. Selvaraj, Joint Managing Director</div>
+        <div className="t3">of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha & Shri. Raj Kumar Singh, Honorable</div>
+        <div className="t3">Minister of State.</div>
+        </div></div></div>
+        <span className="t6">INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION. </span>
+        <div><img src={img3} className="img3"></img></div>
+        <span className="t8">Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors</span>
+        <div className="line"></div>
+        <div className="t7">C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</div>
+        <div className="t6">CHEMICALS & PROCESS<span className="t5">|</span>POWER<span className="t5">|</span>
+         WATER & WASTE WATER<span className="t5">|</span>
+          OILS & GAS<span className="t5">|</span>
+          PHARMA<span className="t5">|</span>
+          SUGARS & DISTILLERIES<span className="t5">|</span>
+           PAPER & PULP<span className="t5">|</span>
+           MARINE & DEFENCE<span className="t5">|</span>
+           METAL & MINING<span className="t5">|</span>
+           <span>FOOD & BEVERAGE<span className="t5">|</span></span></div>
+           <div className="t6"> PETROCHEMICAL & REFINERIES<span className="t5">|</span>
+            SOLAR<span className="t5">|</span>
+            BUILDING HVAC FIRE FIGHTING<span className="t5">|</span>
+             AGRICULTURE & RESIDENTIAL</div>
+            <div className="box"><br></br>
+              <div >
+                
+                <div className="t10">Toll Free <span className="t9">1800 200 1734</span>
+                <span className="t10" >www.facebook.com/cripumps</span>
+                <span className="t10" >www.cripumps.com</span>
+                </div>
+              </div>
+            </div>
+        </div></div>
+
+        
+
+    </div>
+  );
+}
